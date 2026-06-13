66c2e6b8a876725809b717740348968fa0bcb943
try <try.sugiarto05@gmail.com>
2026-06-14 06:05:20 +0700

    fix https force scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,5 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+
+        URL::forceScheme('https');
     }
 }
